7a89929687f79653cd78704cfdb3c930ed8ec339
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-27 18:18:53 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//計算三角形面積
+//計算三角形面積https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6858
 int main(){
     int width, heigth;
     while (scanf("%d %d",&width, &heigth) != EOF) {
